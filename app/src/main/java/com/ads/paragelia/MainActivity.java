5a6e79089f4e63e2990a483c9ed6bacbe6283cb9
Perry3l <151792018+Perry3l@users.noreply.github.com>
2026-03-18 00:55:49 +0200

    Πρωτο με τα βασικα αρχικης

--- app/src/main/java/com/ads/paragelia/MainActivity.java
+++ app/src/main/java/com/ads/paragelia/MainActivity.java
@@ -1,12 +1,13 @@
 package com.ads.paragelia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Βρίσκουμε τα CardView
+        CardView cardNewOrder = findViewById(R.id.cardNewOrder);
+        CardView cardTables = findViewById(R.id.cardTables);
+        CardView cardHistory = findViewById(R.id.cardHistory);
+        CardView cardReports = findViewById(R.id.cardReports);
+
+        // Ορίζουμε listeners
+        cardNewOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ξεκινάμε activity για νέα παραγγελία
+                // Intent intent = new Intent(MainActivity.this, NewOrderActivity.class);
+                // startActivity(intent);
+                Toast.makeText(MainActivity.this, "Νέα Παραγγελία - θα υλοποιηθεί", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cardTables.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Διαχείριση Τραπεζιών", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cardHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Ιστορικό Παραγγελιών", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cardReports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Αναφορές", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
